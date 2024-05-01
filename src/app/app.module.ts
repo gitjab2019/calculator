9ee7844d6a9cb0e16765fcfefbece5c1ff88397c
gitjab2019 <bergues@gmail.com>
2024-05-01 18:05:14 -0300

    Calc with routes menu setted up, see app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VisorComponent } from './visor/visor.component';
 import { BotonComponent } from './boton/boton.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+
+const myRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'features', component: FeaturesComponent },
+  { path: 'pricing', component: PricingComponent },
+  { path: 'aboutus', component: AboutusComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     VisorComponent,
-    BotonComponent
+    BotonComponent,
+    HomeComponent,
+    FeaturesComponent,
+    PricingComponent,
+    AboutusComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(myRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
